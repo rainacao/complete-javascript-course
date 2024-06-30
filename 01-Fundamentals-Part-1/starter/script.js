caeaed3115e2988bab3091cf7c015fb316cefbfe
Raina Cao <imyt.cao@gmail.com>
2024-06-30 14:53:25 -0700

    workkinng

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,70 @@
+// coding practice from https://codingheroes.io/assignments/values-and-variables
+
+// q1
+let country;
+console.log(typeof country);
+country = 0
+console.log(typeof country);
+let continent = "North America";
+let population = 37;
+console.log(country, continent, population);
+
+/* 
+// variable declaration
+let age = 22;
+age = 23;
+console.log(age)
+
+const birthyear = 2001;
+// birthyear = 2000; //results in an error
+
+// don't use var!
+var job = "programmer";
+job = "artist";
+
+lastname = "Cao"; //legal, but not recommended
+console.log(lastname);
+*/
+
+/*
+// math operators
+const now = 2050;
+const ageRai = now - 2001;
+const ageRen = now - 1998;
+console.log(ageRai - ageRen);
+
+// creating two different values in console
+console.log(ageRai * 2, ageRai / 2, 2**3);
+
+const firstName = "rai";
+const lastName = "cao";
+console.log(firstName + " " + lastName);
+
+// assignment operators
+let q = 10 + 5; //q = 15
+q += 5; //q = 20
+q *= 2; //q = 40
+q -= 3; //q = 37
+q /= 2; //q = 18.5
+q++; //q = 19.5
+q--; //q = 18.5
+// q**; //results in an error
+console.log(q);
+
+console.log(ageRen > ageRai);
+console.log(ageRen <= 30);
+
+console.log(now - 2001 > now - 2005);
+*/
+
+/*
+// operator precedence
+// console.log(20 - 4 - 6); // math operators are in a left to right direction
+let x, y;
+x = y;
+y = 20 - 4 - 6;
+console.log(x, y) // undefined 10
+
+x = y = 20 - 4 - 6; // x = y = 10
+console.log(x, y) // 10 10
+*/
